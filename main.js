e50d59d0a03e722e8f66b10904fc7217e615564f
veldan123 <161025728+veldan123@users.noreply.github.com>
2024-06-22 12:18:51 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function b1(){
+
+    window.open("https://shopee.sg/Samyang-hot-chicken-instant-noodles-carbonara-instant-Korea-ramen-supper-Korean-ramyun-fire-hot-spicy-noodle-challenge-i.26337584.22435250616?sp_atk=fb54e1ce-326b-41db-b25b-3826f296be97&xptdk=fb54e1ce-326b-41db-b25b-3826f296be97")
+   
+}
+function b2(){
+    window.open ("https://www.mcdonalds.com.sg/search/node?keys=french%20fries")
+}
+
+function b3(){
+    window.open("https://www.google.com/search?q=fried+rice+shop+near+me&sca_esv=4c1f4100c44949b9&rlz=1C1EJFC_enSG810SG810&sxsrf=ADLYWIKpprmcxuToB129iRTDOAx6EfJfYw%3A1719027354067&ei=mkZ2ZpjeA7zgseMP9ISq6A4&oq=fried+rice+shop+near+me&gs_lp=Egxnd3Mtd2l6LXNlcnAiF2ZyaWVkIHJpY2Ugc2hvcCBuZWFyIG1lKgIIADIKEAAYgAQYQxiKBTIFEAAYgAQyBhAAGBYYHjIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHjIGEAAYFhgeMgYQABgWGB4yCxAAGIAEGIYDGIoFMgsQABiABBiGAxiKBUjGJ1DTBViFHHABeAGQAQCYAVSgAbUEqgEBOLgBAcgBAPgBAZgCCaAC4QTCAgoQABiwAxjWBBhHwgINEAAYsAMY1gQYRxjJA8ICDhAAGIAEGLADGJIDGIoFwgIIEAAYFhgeGA-YAwCIBgGQBgqSBwE5oAegNg&sclient=gws-wiz-serp")
+}
